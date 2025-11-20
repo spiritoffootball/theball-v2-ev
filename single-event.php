4a3bd069f8cbf3b7c54feb2749f7edcc77744a87
Christian Wach <needle@haystack.co.uk>
2025-11-20 10:28:43 +0000

    Remove unnecessary includes

--- single-event.php
+++ single-event.php
@@ -34,14 +34,6 @@ get_header();
 	<?php load_template( $hosts_loop ); ?>
 <?php endif; ?>
 
-<?php if ( $ball_hosts_loop = locate_template( 'template-parts/loop-event-ball-hosts.php' ) ) : ?>
-	<?php load_template( $ball_hosts_loop ); ?>
-<?php endif; ?>
-
-<?php if ( $pledge_form = locate_template( 'template-parts/form-pledge-single.php' ) ) : ?>
-	<?php load_template( $pledge_form ); ?>
-<?php endif; ?>
-
 <?php if ( $sdgs_loop = locate_template( 'template-parts/loop-sdg-linked.php' ) ) : ?>
 	<?php load_template( $sdgs_loop ); ?>
 <?php endif; ?>
