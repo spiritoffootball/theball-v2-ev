268464262c73ad971f70c3895e33f51ca54c3890
Christian Wach <needle@haystack.co.uk>
2026-06-12 10:57:33 +0100

    Bump version to 1.0.1

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // Set our version here.
-define( 'THE_BALL_V2_EV_THEME_VERSION', '1.0.1a' );
+define( 'THE_BALL_V2_EV_THEME_VERSION', '1.0.1' );
 
 /**
  * Bootstraps theme object and returns instance.
